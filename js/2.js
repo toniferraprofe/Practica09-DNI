fd35bada108e8f7eed14bf69aad84d5b71507eba
toniferraprofe <52382398+toniferraprofe@users.noreply.github.com>
2020-05-28 18:17:47 +0200

    Cambios para que funcione

--- js/2.js
+++ js/2.js
@@ -1,66 +1,47 @@
-var numeroDni = Number(prompt("Escribe los numeros del dni"));
-var divideDNI23 = (numeroDni / 23);
-//var solo2decimales = divideDNI23.toFixed(2); //no me ha funcionado
-console.log(calculaLetraDNI);
+// VARIABLES
+var numeroDni = prompt("Escribe los numeros del dni", "Introduce 8 números");
+
+// Se busca el resto de la división 
+var resto = (numeroDni % 23);
+
+// en lugar de un obejeto más fácil un array
+var letrasDni = [
+"T",
+"R",
+"W",
+"A",
+"G",
+"M",
+"Y",
+"F",
+"P",
+"D",
+"X",
+"B",
+"N",
+"J",
+"Z",
+"S",
+"Q",
+"V",
+"H",
+"L",
+"C",
+"K",
+"E",
+"T"
+]
+
+
+// RESULTADO
+
+var resultado = `numeroDni = ${numeroDni} - ${letrasDni[resto]}`;
+
+document.write(resultado);
+console.log(resultado);
 
 
 
-function divide (numeroDni, num23) {
-	console.log(a + b);
-}
-
-
-
-
-
-
-// incluir una variable dentro de un string: Cambiar comillas por este símbolo '`' y utilizar ${} para retener la variable dentro
-function suma(a, b) {
-	console.log(`El resultado de la suma es ${a + b}`);
-}
-
---------------------------------
-
-document.write(`numeroDni = ${numeroDni}, n2 = ${n2}`);
-
-
-
-var divideDNI23 = Math.round(numeroDni / 23);
-var solo2decimales = divideDNI23.toFixed(2);
-
-
-
-console.log(divideDNI23);
-
-
-var letrasDni = {
-
-resto1 : "T",
-resto2 : "R",
-resto3 : "W",
-resto4 : "A",
-resto5 : "G",
-resto6 : "M",
-resto7 : "Y",
-resto8 : "F",
-resto9 : "P",
-resto10 : "D",
-resto11 : "X",
-resto12 : "B",
-resto13 : "N",
-resto14 : "J",
-resto15 : "Z",
-resto16 : "S",
-resto17 : "Q",
-resto18 : "V",
-resto19 : "H",
-resto20 : "L",
-resto21 : "C",
-resto22 : "K",
-resto23 : "E",
-resto24 : "T",
-
-}
 
 
 
